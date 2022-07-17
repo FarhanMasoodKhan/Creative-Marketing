34381b274c8e5c2fff86503baba08d2b297862e5
FarhanMasood <farhanmasoodkhan2@gmail.com>
2022-07-17 14:47:34 +0500

    Day 3,4

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,4 @@
-// $(document).ready(function() {
-
-//   $(window).scroll(function() {
-
-//       var height = $('.first-container').height();
-//       var scrollTop = $(window).scrollTop();
-
-//       if (scrollTop >= height - 40) {
-//           $('.nav-container').addClass('solid-nav');
-//       } else {
-//           $('.nav-container').removeClass('solid-nav');
-//       }
-
-//   });
-// });
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
