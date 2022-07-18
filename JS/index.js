bc0937454512d64a71cb1c7b712221b0dfb42389
FarhanMasood <farhanmasoodkhan2@gmail.com>
2022-07-18 15:48:04 +0500

    Day 6 - 18 July

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,38 @@
+//  Dark mode Part
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
   }
+
+  // Slider Part
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].className = slides[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    slides[slideIndex-1].className += " active";
+  }
+  window.onload= function () {
+   setInterval(function(){ 
+       plusSlides(1);
+   }, 3000);
+   }
+  
